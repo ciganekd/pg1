373dbf2377c88af6860b558816587674cc8682bd
ciganekd <cigand00@jcu.cz>
2025-10-02 09:01:14 +0000

    prvni_program

---
+++ first.py
@@ -0,0 +1,7 @@
+#vytvořte si účet na https://github.com
+#vytvořte projekt pg
+#vytvořte svůj první program first.py
+#program first.py bude obsahovat jednu funkci sudy_nebo_lichy a bude přebírat jeden parametr cislo, pomocí podmínky otestuje, zda je liché nebo sudé a vypíše text: "Číslo X je sudé" nebo "Číslo X je liché"
+#do programu přidejte volání funkce: sudy_nebo_lichy(5) a sudy_nebo_lichy(1000000)
+#nahrajte ho do repozitáře
+#odkaz na váš repozitář nahrajte zde
