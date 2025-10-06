2e34cfd79de93dffc36d1739d9dcd6a3f8bb92c3
ciganekd <cigand00@jcu.cz>
2025-10-06 12:26:07 +0200

    Update first.py

--- first.py
+++ first.py
@@ -5,3 +5,13 @@
 #do programu přidejte volání funkce: sudy_nebo_lichy(5) a sudy_nebo_lichy(1000000)
 #nahrajte ho do repozitáře
 #odkaz na váš repozitář nahrajte zde
+
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
+
+# Volání funkcí
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
